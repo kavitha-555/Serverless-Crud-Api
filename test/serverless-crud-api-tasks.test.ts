4dc71dd053701d034b63553d81d11c7e4312bdf0
root <root@ip-172-31-30-219.ec2.internal>
2024-12-21 05:01:55 +0000

    Initial commit

---
+++ test/serverless-crud-api-tasks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessCrudApiTasks from '../lib/serverless-crud-api-tasks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-crud-api-tasks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessCrudApiTasks.ServerlessCrudApiTasksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
